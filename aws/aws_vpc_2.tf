0b3fa5a03d31e695d41985f0c3fb3255ae7b00ce
deo10 <pipazoglov@gmail.com>
2023-06-07 14:46:18 +0600

    adding new vpc config, variables - completed #3 day class

--- aws/aws_vpc_2.tf
+++ aws/aws_vpc_2.tf
@@ -3,46 +3,54 @@ provider "aws" {
   region = "us-east-1"  # Change to your desired region
 }
 
+# Define az
 locals {
-  public_cidr = ["10.0.0.0/24", "10.0.1.0/24"]
-  private_cidr = ["10.0.2.0/24", "10.0.3.0/24"]
+
   availability_zones = ["us-east-1a", "us-east-1b"]
 }
 
 # Create the VPC
 resource "aws_vpc" "my_vpc" {
-  cidr_block = "10.0.0.0/16"  # Replace with your desired VPC CIDR block
+  cidr_block = var.vpc_cidr  # will create vpc cidr from variables.tf
+
+  tags = {
+    Name = var.env_code # will create tag from variables.tf
+  }
 }
 
 # Create the internet gateway
 resource "aws_internet_gateway" "my_igw" {
   vpc_id = aws_vpc.my_vpc.id
+
+  tags = {
+    Name = var.env_code # will create tag from variables.tf
+  }
 }
 
 # Create the public subnets
 resource "aws_subnet" "public" {
-  count                   = 2
+  count                   = length(var.public_cidr) # will count number of values in locals-public_cidr
 
   vpc_id                  = aws_vpc.my_vpc.id
-  cidr_block              = local.public_cidr[count.index]  # Replace with your desired public subnet CIDR block for AZ1
-  availability_zone       = local.availability_zones[count.index]  # Replace with your desired AZ1
+  cidr_block              = var.public_cidr[count.index]  # will use values in locals-public_cidr
+  availability_zone       = local.availability_zones[count.index]  # will use values in locals-azs
   map_public_ip_on_launch = true
 
   tags = {
-    Name = "public${count.index+1}"
+    Name = "${var.env_code}-public${count.index+1}" # will create tag env_code+public+number
   }
 }
 
 # Create the private subnets
 resource "aws_subnet" "private" {
-  count                   = 2
+  count                   = length(var.private_cidr)
 
   vpc_id                  = aws_vpc.my_vpc.id
-  cidr_block              = local.private_cidr[count.index]  # Replace with your desired private subnet CIDR block for AZ1
-  availability_zone       = local.availability_zones[count.index]  # Replace with your desired AZ1
+  cidr_block              = var.private_cidr[count.index]  # will use values in locals-public_cidr
+  availability_zone       = local.availability_zones[count.index]  # will use values in locals-azs
 
   tags = {
-    Name = "private${count.index+1}"
+    Name = "${var.env_code}-private${count.index+1}"
   }
 }
 
@@ -54,45 +62,69 @@ resource "aws_route_table" "public" {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.my_igw.id
   }
+
+  tags = {
+    Name = "${var.env_code}-public${count.index+1}" # will create tag env_code+public+number
+  }
 }
 
 resource "aws_route_table" "private" {
-  count = 2
+  count = length(var.private_cidr)
   vpc_id = aws_vpc.my_vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
     nat_gateway_id = aws_nat_gateway.nat_gateway[count.index].id
   }
+
+  tags = {
+    Name = "${var.env_code}-private${count.index+1}"
+  }
 }
 
 # Associate the subnets with the route tables
 resource "aws_route_table_association" "public" {
-  count          = 2
+  count          = length(var.public_cidr)
 
   subnet_id      = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.public.id
+
+  tags = {
+    Name = "${var.env_code}-public${count.index+1}"
+  }
 }
 
 resource "aws_route_table_association" "private" {
-  count          = 2
+  count          = length(var.private_cidr)
 
   subnet_id      = aws_subnet.private[count.index].id
   route_table_id = aws_route_table.private[count.index].id
+
+  tags = {
+    Name = "${var.env_code}-private${count.index+1}"
+  }
 }
 
 # Create the Elastic IPs for the NAT gateways
 resource "aws_eip" "nat" {
-  count = 2
+  count = length(var.public_cidr)
   
   vpc   = true
+
+  tags = {
+    Name = "${var.env_code}-eip${count.index+1}"
+  }
 }
 
 # Create the NAT gateways
 resource "aws_nat_gateway" "nat_gateway" {
-  count         = 2
+  count         = length(var.public_cidr)
 
   allocation_id = aws_eip.nat[count.index].id
   subnet_id     = aws_subnet.public[count.index].id
+
+  tags = {
+    Name = "${var.env_code}-nat${count.index+1}"
+  }
 }
 
