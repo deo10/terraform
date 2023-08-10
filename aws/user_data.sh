322ecac76fdee0d480e3fa386ec2c7d62e6b6f07
deo10 <pipazoglov@gmail.com>
2023-08-10 15:45:30 +0600

    install 2048 game via user data

--- aws/user_data.sh
+++ aws/user_data.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 sudo yum update -y
-sudo yum install -y httpd
+sudo yum install -y httpd git
+git clone https://github.com/gabrielecirulli/2048.git
+cp -R 2048/* /var/www/html
 sudo systemctl start httpd && sudo systemctl enable httpd
