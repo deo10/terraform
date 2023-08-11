0db394c2c155b2af910f1b2f9cf87a263bc015d2
Andrei Panov <32164262+deo10@users.noreply.github.com>
2023-08-11 20:56:53 +0600

    Day7 task1 (#8)
    * Configure remote state
    
    * remote state resources added
    
    * minor change

--- aws/provider.tf
+++ aws/provider.tf
@@ -1,3 +1,13 @@
+#Making tf state remote on S3, db to lock state, both created by files in remote-state folder
+terraform {
+  backend "s3" {
+    bucket = "my-tf-test-bucket"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+    dynamodb_table = "terraform-remote-state"
+  }
+}
+
 # Define the provider (AWS in this case)
 provider "aws" {
   region = "us-east-1" # Change to your desired region
