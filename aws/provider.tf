c60f91ae68c2c66b25cf3433abfbd82f377a7a00
deo10 <pipazoglov@gmail.com>
2023-08-11 16:37:08 +0600

    Configure remote state

--- aws/provider.tf
+++ aws/provider.tf
@@ -1,3 +1,12 @@
+#Making tf state remote on S3, bucket created manually
+terraform {
+  backend "s3" {
+    bucket = "bucket_name"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
 # Define the provider (AWS in this case)
 provider "aws" {
   region = "us-east-1" # Change to your desired region
