4053697797122818c8a1cd23f7425498172a3ad7
Andrei Panov <32164262+deo10@users.noreply.github.com>
2023-08-27 17:18:50 +0600

    addind loadbalancer and forward traffic to the private instances (#10)

--- level0/main.tf
+++ level0/main.tf
@@ -9,7 +9,7 @@ resource "aws_dynamodb_table" "state-lock" {
   read_capacity  = 1
   write_capacity = 1
   hash_key       = "LockID"
-  
+
   attribute {
     name = "LockID"
     type = "S"
--- level2/instances.tf
+++ level2/instances.tf
@@ -13,13 +13,15 @@ data "aws_ami" "amazonlinux" { #looking for region specific ami and filter on it
 }
 
 resource "aws_instance" "public" {
+  count = 2
+
   ami                         = data.aws_ami.amazonlinux.id #using data resource to get region specific ami id
   instance_type               = "t3.micro"
   associate_public_ip_address = true
   key_name                    = "main" #key pair name (created manually)
   vpc_security_group_ids      = [aws_security_group.public.id]
-  subnet_id                   = data.terraform_remote_state.level1.outputs.public_subnet_id[0] #using existing subnet from outputs in level1
-  user_data                   = file["user_data.sh"]    #apply sh script on ec2 instance
+  subnet_id                   = data.terraform_remote_state.level1.outputs.public_subnet_id[count.index] #using existing subnet from outputs in level1
+
 
 
   tags = {
@@ -41,7 +43,15 @@ resource "aws_security_group" "public" {
   }
 
   ingress {
-    description = "Webserver access HTTP from public"
+    description     = "Webserver access HTTP from public"
+    from_port       = 80 #Adding a port range from to
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.load-balancer.id]
+  }
+
+  ingress {
+    description = "Webserver access HTTP from loadbalancer"
     from_port   = 80 #Adding a port range from to
     to_port     = 80
     protocol    = "tcp"
@@ -61,11 +71,14 @@ resource "aws_security_group" "public" {
 }
 
 resource "aws_instance" "private" {
+  count = 2
+
   ami                    = data.aws_ami.amazonlinux.id
   instance_type          = "t3.micro"
   key_name               = "main" #key pair name (created manually)
   vpc_security_group_ids = [aws_security_group.private.id]
-  subnet_id              = data.terraform_remote_state.level1.outputs.private_subnet_id[0] #using existing subnet from aws_vpc
+  subnet_id              = data.terraform_remote_state.level1.outputs.private_subnet_id[count.index] #using existing subnet from aws_vpc
+  user_data              = file["user_data.sh"]                                                      #apply sh script on ec2 instance
 
 
   tags = {
@@ -83,6 +96,14 @@ resource "aws_security_group" "private" {
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = [data.terraform_remote_state.level1.outputs.vpc_cidr] #using VPC cidr from outputs level1
+  }
+
+  ingress {
+    description     = "Webserver access HTTP from loadbalancer"
+    from_port       = 80 #Adding a port range from to
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.load-balancer.id]
   }
 
   egress {
