a27b1d03bba4053b7410a16be0a9305664e38b96
Andrei Panov <32164262+deo10@users.noreply.github.com>
2023-08-30 12:18:49 +0600

    Day10_Adding autoscaling group (#11)

--- level2/instances.tf
+++ level2/instances.tf
@@ -117,3 +117,32 @@ resource "aws_security_group" "private" {
     Name = "${var.env_code}-private"
   }
 }
+
+#Autoscaling
+
+resource "aws_launch_configuration" "main" {
+  name_prefix     = "${var.env_code}-private"
+  image_id        = data.aws_ami.amazonlinux
+  instance_type   = "t2.micro"
+  security_groups = [aws_security_group.private]
+  user_data       = file("user_data.sh")
+  key_name        = "main"
+}
+
+resource "aws_autoscaling_group" "main" {
+  name             = "${var.env_code}-private"
+  min_size         = 2
+  desired_capacity = 2
+  max_size         = 4
+
+  target_group_arns    = [aws_lb_target_group.main.arn] #all instances are under loadbalancer
+  launch_configuration = aws_launch_configuration.main.name
+  vpc_zone_identifier  = data.terraform_remote_state.level1.outputs.private_subnet_id #created in private subnet
+
+  #adding tags on instances that is lauching
+  tags = {
+    key                 = "Name"
+    value               = var.env_code
+    propagate_at_launch = true
+  }
+}
