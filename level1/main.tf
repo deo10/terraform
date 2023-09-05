497a89403556c9a2853399e914693ee2351cd943
deo10 <pipazoglov@gmail.com>
2023-09-05 16:45:23 +0600

    moving to modules

--- level1/main.tf
+++ level1/main.tf
@@ -3,122 +3,14 @@ data "aws_availability_zones" "available" { #taking all available AZ from the re
   state = "available"
 }
 
-# Create the VPC
-resource "aws_vpc" "my_vpc" {
-  cidr_block = var.vpc_cidr # will create vpc cidr from variables.tf
+#creating VPC using modules
+module "vpc" {
+  source = "../modules/vpc"
 
-  tags = {
-    Name = var.env_code # will create tag from variables.tf
-  }
-}
-
-# Create the internet gateway
-resource "aws_internet_gateway" "my_igw" {
-  vpc_id = aws_vpc.my_vpc.id
-
-  tags = {
-    Name = var.env_code # will create tag from variables.tf
-  }
-}
-
-# Create the public subnets
-resource "aws_subnet" "public" {
-  count = length(var.public_cidr) # will count number of values in locals-public_cidr
-
-  vpc_id                  = aws_vpc.my_vpc.id
-  cidr_block              = var.public_cidr[count.index]                             # will use values in locals-public_cidr
-  availability_zone       = data.aws_availability_zones.available.names[count.index] # will use values from data az
-  map_public_ip_on_launch = true
-
-  tags = {
-    Name = "${var.env_code}-public${count.index + 1}" # will create tag env_code+public+number
-  }
-}
-
-# Create the private subnets
-resource "aws_subnet" "private" {
-  count = length(var.private_cidr)
-
-  vpc_id            = aws_vpc.my_vpc.id
-  cidr_block        = var.private_cidr[count.index]                            # will use values in locals-private_cidr
-  availability_zone = data.aws_availability_zones.available.names[count.index] # will use values in date-azs
-
-  tags = {
-    Name = "${var.env_code}-private${count.index + 1}"
-  }
-}
-
-# Create the route tables
-resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.my_vpc.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.my_igw.id
-  }
-
-  tags = {
-    Name = "${var.env_code}-public${count.index + 1}" # will create tag env_code+public+number
-  }
-}
-
-resource "aws_route_table" "private" {
-  count  = length(var.private_cidr)
-  vpc_id = aws_vpc.my_vpc.id
-
-  route {
-    cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.nat_gateway[count.index].id
-  }
-
-  tags = {
-    Name = "${var.env_code}-private${count.index + 1}"
-  }
-}
-
-# Associate the subnets with the route tables
-resource "aws_route_table_association" "public" {
-  count = length(var.public_cidr)
-
-  subnet_id      = aws_subnet.public[count.index].id
-  route_table_id = aws_route_table.public.id
-
-  tags = {
-    Name = "${var.env_code}-public${count.index + 1}"
-  }
-}
-
-resource "aws_route_table_association" "private" {
-  count = length(var.private_cidr)
-
-  subnet_id      = aws_subnet.private[count.index].id
-  route_table_id = aws_route_table.private[count.index].id
-
-  tags = {
-    Name = "${var.env_code}-private${count.index + 1}"
-  }
-}
-
-# Create the Elastic IPs for the NAT gateways
-resource "aws_eip" "nat" {
-  count = length(var.public_cidr)
-
-  vpc = true
-
-  tags = {
-    Name = "${var.env_code}-eip${count.index + 1}"
-  }
-}
-
-# Create the NAT gateways
-resource "aws_nat_gateway" "nat_gateway" {
-  count = length(var.public_cidr)
-
-  allocation_id = aws_eip.nat[count.index].id
-  subnet_id     = aws_subnet.public[count.index].id
-
-  tags = {
-    Name = "${var.env_code}-nat${count.index + 1}"
-  }
+  env_code           = var.env_code
+  vpc_cidr           = var.vpc_cidr
+  public_cidr        = var.public_cidr
+  private_cidr       = var.private_cidr
+  availability_zones = data.aws_availability_zones.available.names
 }
 
---
+++ modules/asg/main.tf
@@ -0,0 +1,148 @@
+#creating EC2 instances
+
+resource "aws_security_group" "public" {
+  name        = "${var.env_code}-public"
+  description = "Allow inbound traffic"
+  vpc_id      = var.vpc_id #using vpc from module config
+
+  /*   ingress {
+    description = "SSH from public"
+    from_port   = 22 #Adding a port range from to
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["109.201.178.180/32"] #check and add your public IP here 
+  } */ #commented as a part of implementing Session Manager
+
+  ingress {
+    description     = "Webserver access HTTP from public"
+    from_port       = 80 #Adding a port range from to
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = var.lb_security_group_id
+  }
+
+  ingress {
+    description = "Webserver access HTTP from loadbalancer"
+    from_port   = 80 #Adding a port range from to
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] #open widly
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.env_code}-public"
+  }
+}
+
+resource "aws_instance" "private" {
+  count = 2
+
+  ami                    = var.ami_id
+  instance_type          = "t3.micro"
+  key_name               = "main" #key pair name (created manually)
+  vpc_security_group_ids = [aws_security_group.private.id]
+  subnet_id              = var.private_subnet_id[count.index]  #using existing subnet from aws_vpc
+  user_data              = file["$(path.module)/user_data.sh"] #apply sh script on ec2 instance
+
+
+  tags = {
+    Name = "${var.env_code}-private" #using env var from variables.tf
+  }
+}
+resource "aws_security_group" "private" {
+  name        = "${var.env_code}-private"
+  description = "Allow VPC traffic"
+  vpc_id      = var.vpc_id #using vpc from module config
+
+  /* ingress {
+    description = "SSH from VPC"
+    from_port   = 22 #Adding a port range from to
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [data.terraform_remote_state.level1.outputs.vpc_cidr] #using VPC cidr from outputs level1
+  } */ # commented as a part of implementing Session Manager
+
+  ingress {
+    description     = "Webserver access HTTP from loadbalancer"
+    from_port       = 80 #Adding a port range from to
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = var.lb_security_group_id
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.env_code}-private"
+  }
+}
+
+#Autoscaling
+
+resource "aws_launch_configuration" "main" {
+  name_prefix          = "${var.env_code}-private"
+  image_id             = var.ami_id
+  instance_type        = "t2.micro"
+  security_groups      = [aws_security_group.private]
+  user_data            = file("user_data.sh")
+  iam_instance_profile = aws_iam_instance_profile.main.name #as a part of implementing Session Manager
+  #key_name        = "main" #as a part of implementing Session Manager
+}
+
+resource "aws_autoscaling_group" "main" {
+  name             = "${var.env_code}-private"
+  min_size         = 2
+  desired_capacity = 2
+  max_size         = 4
+
+  target_group_arns    = var.target_group_arn #all instances are under loadbalancer
+  launch_configuration = aws_launch_configuration.main.name
+  vpc_zone_identifier  = var.private_subnet_id #created in private subnet
+
+  #adding tags on instances that is lauching
+  tags = {
+    key                 = "Name"
+    value               = var.env_code
+    propagate_at_launch = true
+  }
+}
+
+
+#IAM part
+resource "aws_iam_role" "main" {
+  name                      = var.env_code
+  managed_policy_arns       = ["arn:aws:iam:aws:policy/service-role/AmazonEC2RoleforSSM"]
+  assume_assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+  EOF  
+}
+
+
+resource "aws_iam_instance_profile" "main" {
+  name = var.env_code
+  role = aws_iam_role.main.name
+}
