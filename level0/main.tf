6eea11d7b5670391028daca175b540ab479c2d0e
deo10 <pipazoglov@gmail.com>
2023-09-10 19:34:18 +0600

    changes on bucket name and elements that are not allowed on KK

--- level0/main.tf
+++ level0/main.tf
@@ -1,17 +1,17 @@
 #Automate creation of S3 bucket for remote state
 resource "aws_s3_bucket" "remote-state" {
-  bucket = "my-tf-test-bucket"
+  bucket = "my-tf-test-bucket-panov"
 }
 
-resource "aws_dynamodb_table" "state-lock" {
-  name           = "terraform-remote-state"
-  billing_mode   = "PROVISIONED"
-  read_capacity  = 1
-  write_capacity = 1
-  hash_key       = "LockID"
+# resource "aws_dynamodb_table" "state-lock" {
+#   name           = "terraform-remote-state"
+#   billing_mode   = "PROVISIONED"
+#   read_capacity  = 1
+#   write_capacity = 1
+#   hash_key       = "LockID"
 
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-}
+#   attribute {
+#     name = "LockID"
+#     type = "S"
+#   }
+# }
