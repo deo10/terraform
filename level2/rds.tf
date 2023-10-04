686879462f9c6cd294a4e1dbb881ab0f4fdaf84a
Andrei Panov <pipazoglov@gmail.com>
2023-10-04 20:16:10 +0300

    adding rds module and use of aws secret manager

---
+++ level2/rds.tf
@@ -0,0 +1,23 @@
+#using aws secret manager and manually created secret for RDS
+data "aws_secretmanager_secret" "rds_password" {
+    name = "main/rds/password"
+}
+data "aws_secretmanager_secret_version" "rds_password" {
+    secret_id = data.aws_secretmanager_secret.rds_password.id
+}
+#password will be decoded, to decode use
+locals {
+    rds_password = jsondecode(data.aws_secretmanager_secret_version.rds_password.secret_string)["password"]
+}
+
+module "rds" {
+  source = "../modules/rds"
+
+  subnet_ids            = data.terraform_remote_state.level1.outputs.private_subnet_id
+  env_code              = var.env_code
+  rds_password          = local.rds_password
+  vpc_id                = data.terraform_remote_state.level1.outputs.vpc_id
+  source_security_group = module.asg.security_group_id
+
+
+}
